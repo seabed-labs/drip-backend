84e18e1896b642df50fced835006de79717cc4dd
Mocha Dcaf <mocha@dcaf.so>
2022-09-28 10:04:25 -0400

    chore: increase rate limit for devnet

--- pkg/service/clients/solana/client.go
+++ pkg/service/clients/solana/client.go
@@ -460,7 +460,7 @@ func GetRequestsPerSecondLimit(network configs.Network, primary bool) int {
 	case configs.MainnetNetwork:
 		return 30
 	case configs.DevnetNetwork:
-		return 30
+		return 15
 	default:
 		return 3
 	}
