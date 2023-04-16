c7a86e3b7bc76cf11a92d0d4089af90581ef2528
Mocha Dcaf <mocha@dcaf.so>
2023-04-16 13:48:26 -0400

    test: add test for dripOrcaWhirlpool

--- pkg/service/ixparser/ix_parser_test.go
+++ pkg/service/ixparser/ix_parser_test.go
@@ -1,16 +1,16 @@
 package ixparser
 
 import (
+	"context"
 	"testing"
 
-	"github.com/test-go/testify/assert"
-
 	"github.com/dcaf-labs/drip/pkg/unittest"
-
+	drip "github.com/dcaf-labs/solana-drip-go/pkg"
 	api "github.com/dcaf-labs/solana-go-retryable-http-client"
-
+	"github.com/gagliardetto/solana-go"
 	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/gagliardetto/solana-go/rpc/jsonrpc"
+	"github.com/test-go/testify/assert"
 )
 
 func TestIxParser(t *testing.T) {
@@ -24,4 +24,27 @@ func TestIxParser(t *testing.T) {
 	}
 	rpcClient := rpc.NewWithCustomRPCClient(jsonrpc.NewClientWithOpts(url, opts))
 	assert.NotNil(t, rpcClient)
+
+	parser := ixParser{}
+
+	t.Run("can parse v1 dripOrcaWhirlpool ix", func(t *testing.T) {
+		txRaw, err := rpcClient.GetTransaction(context.Background(), solana.MustSignatureFromBase58("5uECxzjML1a5sXuMPqwcpP8BMAKCXCpUVwFydPHWBnaW9V72ockWQKevfSQBaqiSxtGKtmstkVaxmo5Jcfnp9Lcb"), nil)
+		assert.NoError(t, err)
+		assert.NotNil(t, txRaw)
+		assert.NotNil(t, txRaw.Transaction)
+		assert.Equal(t, drip.GetIdlVersion(txRaw.Slot), 1)
+
+		tx, err := txRaw.Transaction.GetTransaction()
+		assert.NoError(t, err)
+		assert.Len(t, tx.Message.Instructions, 2)
+
+		ix := tx.Message.Instructions[1]
+		accounts, err := ix.ResolveInstructionAccounts(&tx.Message)
+		assert.NoError(t, err)
+		parsedIx, err := parser.MaybeParseV1DripOrcaWhirlpool(accounts, ix.Data)
+		assert.NoError(t, err)
+		assert.NotNil(t, parsedIx)
+		assert.Equal(t, parsedIx.GetDripOrcaWhirlpoolAccounts().Common.Vault.String(), "6PnzoW2Bcs6WGqYvecfSxN9C2EeDmQCjUCeFA7JDDfZG")
+		assert.Equal(t, parsedIx.GetDripOrcaWhirlpoolAccounts().Whirlpool.String(), "HJPjoWUrhoZzkNfRpHuieeFk9WcZWjwy6PBjZ81ngndJ")
+	})
 }
